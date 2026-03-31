dd12195b3707146d7137ed0955393ac5a3342808
Carlos Alloatti <calloatti@gmail.com>
2026-03-31 12:08:50 -0300

    Add project files.

---
+++ ModStarter.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+using UnityEngine;
+
+namespace Calloatti.NoCameraShift
+{
+  public class ModStarter : IModStarter
+  {
+    private const string HarmonyId = "calloatti.nocamerashift";
+
+    public void StartMod(IModEnvironment modEnvironment)
+    {
+      // Instantiate Harmony and apply all patches in this assembly
+      Harmony harmony = new Harmony(HarmonyId);
+      harmony.PatchAll(typeof(ModStarter).Assembly);
+      Debug.Log($"[{HarmonyId}] Harmony patches applied successfully!");
+    }
+  }
+}
